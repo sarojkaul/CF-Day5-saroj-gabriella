df5f1e311d0b91ae73e24904c8a8f6490b8e4468
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2020-11-02 23:22:37 +0100

    night work ;-))

--- src/Gabriella/ExperimentArList.java
+++ src/Gabriella/ExperimentArList.java
@@ -2,8 +2,53 @@ package Gabriella;
 import java.util.*;
 
 public class ExperimentArList {
+    public static void main(String[] args) {
 
-    ArrayList < String > arrayList = new ArrayList < String > ();
-    ArrayList < String > arrL = new arrL < String > ();
+        // create array list
+        // ArrayList < String > arrayList = new ArrayList < String > ();
+        ArrayList<String> menu = new ArrayList<String>();
+
+        menu.add("cheese cake");
+        menu.add("hot chili");
+        menu.add("vine soup");
+        menu.add("Steak");
+        menu.add("schoko mousse");
+        menu.add("selleri saft");
+        menu.add("pizza pepperoni");
+
+        for (int i=0; i<menu.size(); i++){
+            System.out.println( "Element"+i+"  "+ menu.get(i));
+        }
+        System.out.println("****************listing length of elements**************************");
+        for (String string : menu){
+           System.out.println(string+"  "+string.length());
+        }
+
+        menu.set(0,"Strawberry Cheese Cake");
+
+        System.out.println("*****************remove element3*****************************");
+        menu.remove(3);
+        for (int i=0; i<menu.size(); i++){
+            System.out.println( "Element"+i+"  "+ menu.get(i));
+        }
+
+        System.out.println("*****************sort elements*****************************");
+        Collections.sort(menu);
+        for (int i=0; i<menu.size(); i++){
+            System.out.println( "Element"+i+"  "+ menu.get(i)+"     iterator=     "+menu.iterator());
+        }
+        System.out.println("****************try out index of method************");
+        System.out.println(menu.indexOf("vine soup"));
+
+        menu.clear();
+
+
+
+
+
+
+    }
+
+
 
 }
