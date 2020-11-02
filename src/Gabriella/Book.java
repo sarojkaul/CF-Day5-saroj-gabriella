ba1f622c51e531a3257f0691007ca8ae66520301
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2020-11-02 15:19:36 +0100

    second commitment

--- src/Gabriella/Book.java
+++ src/Gabriella/Book.java
@@ -2,16 +2,16 @@ package Gabriella;
 
 public class Book {
 
-    String titel;
+    String title;
     String writer;
     String publisher;
     int publYear;
     String ISBN;
     int numOfBooks;  //this is the number of book right now in the library
-    int lent;  //this is what people lent.  numOfBooks+lent= the whole amount of book
+    int lent;        //this is what people lent.  numOfBooks+lent= the whole amount of book
 
-    public Book(String titel, String writer, String publisher, int publYear, String ISBN, int numOfBooks, int lent) {
-        this.titel = titel;
+    public Book(String title, String writer, String publisher, int publYear, String ISBN, int numOfBooks, int lent) {
+        this.title = title;
         this.writer = writer;
         this.publisher = publisher;
         this.publYear = publYear;
--- src/Gabriella/Library.java
+++ src/Gabriella/Library.java
@@ -3,7 +3,10 @@ package Gabriella;
 public class Library {
     public static void main(String[] args) {
 
-
+        Book b1= new Book("Eine billion Dollar", "Andreas Esbach", "Bastei Lübbe", 2004, "3-404-15040-6", 4,2);
+        Book b1= new Book("Englisch Dictionary", "", "Oxford University", 2018, "3-404-15123-6", 10,10);
+        Book b1= new Book("Gier", "Marc Elsberg", "Bastei Lübbe", 2004, "3-404-15040-6", 4,2);
+        Book b1= new Book("Eine billion Dollar", "Andreas Esbach", "Bastei Lübbe", 2004, "3-404-15040-6", 4,2);
 
 
     }
